4517832711852811e862b92e05561c6515a79513
Mehran <mehran.hrajabi98@gmail.com>
2020-11-30 04:38:46 +0330

    Add onclick functions for faq page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
 function openMenu() {
-    document.getElementById("menu").style.height = "100%";
+  document.getElementById("menu").style.height = "100%";
 }
   
-  function closeMenu() {
-    document.getElementById("menu").style.height = "0%";
-  }
+function closeMenu() {
+  document.getElementById("menu").style.height = "0%";
+}
+  
+
+function pharmacyCollapse(){
+  
+  var pharmacyElement = document.getElementById("pharmacy");
+  var pharmacyItemElement = document.getElementById("pharmacyItem");
+  var shopElement = document.getElementById("shop");
+  var shopItemElement = document.getElementById("shopItem");
+
+  shopElement.style.display = "none" ;
+  shopItemElement.style.opacity = "0.3" ;
+  pharmacyElement.style.display = "initial"
+  pharmacyItemElement.style.opacity = "1";
+}
+
+function shoppingCollapse(){
+  var pharmacyElement = document.getElementById("pharmacy");
+  var pharmacyItemElement = document.getElementById("pharmacyItem");
+  var shopElement = document.getElementById("shop");
+  var shopItemElement = document.getElementById("shopItem");
+
+  pharmacyElement.style.display = "none" ;
+  pharmacyItemElement.style.opacity = "0.3" ;
+  shopElement.style.display = "initial" ;
+  shopItemElement.style.opacity = "1";
+}
